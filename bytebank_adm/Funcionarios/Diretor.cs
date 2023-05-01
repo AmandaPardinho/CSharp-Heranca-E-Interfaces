a55251c2334dd47918d3d1f2cda875b1fa3b156d
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-01 20:48:49 -0300

    Herdando a classe Funcionários

--- bytebank_adm/Funcionarios/Diretor.cs
+++ bytebank_adm/Funcionarios/Diretor.cs
@@ -6,13 +6,8 @@ using System.Threading.Tasks;
 
 namespace bytebank_adm.Funcionarios
 {
-    public class Diretor
+    public class Diretor: Funcionario
     {
-        public string Nome { get; set; }
-        public string Cpf { get; set; }
-        public double Salario { get; set; }
-
-
         public double GetBonificacao()
         {
             return this.Salario;
