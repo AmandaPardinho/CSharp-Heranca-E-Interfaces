2e813fa095b47a558582d56ba42be81e0e2a2770
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-01 16:33:45 -0300

    Criação da parte 2 do projeto bytebank

---
+++ bytebank_adm/Program.cs
@@ -0,0 +1,25 @@
+﻿using bytebank_adm.Funcionarios;
+using bytebank_adm.Utilitario;
+
+Funcionario pedro = new Funcionario();
+pedro.Nome = "Pedro Malazartes";
+pedro.Cpf = "123456789-00";
+pedro.Salario = 2000.0;
+
+Console.WriteLine($"Nome: {pedro.Nome}\nCPF: {pedro.Cpf}\nSalário: {String.Format("{0:0.00}", pedro.Salario)}\nBonificação: {String.Format("{0:0.00}", pedro.GetBonificacao())}");
+
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta Silva";
+roberta.Cpf = "987654321-00";
+roberta.Salario = 5000.0;
+
+Console.WriteLine();
+Console.WriteLine($"Nome: {roberta.Nome}\nCPF: {roberta.Cpf}\nSalário: {String.Format("{0:0.00}", roberta.Salario)}\nBonificação: {String.Format("{0:0.00}", roberta.GetBonificacao())}");
+
+GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+gerenciador.Registrar(pedro);
+gerenciador.Registrar(roberta);
+
+Console.WriteLine($"Total de bonificações: {String.Format("{0:0.00} ", gerenciador.TotalBonificacao)}");
+
+Console.ReadKey();
---
+++ bytebank_adm/Utilitario/GerenciadorBonificacao.cs
@@ -0,0 +1,24 @@
+﻿using bytebank_adm.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_adm.Utilitario
+{
+    public class GerenciadorBonificacao
+    {
+        public double TotalBonificacao { get; private set; }
+
+        public void Registrar(Funcionario funcionario)
+        {
+            this.TotalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+            this.TotalBonificacao += diretor.GetBonificacao();
+        }
+    }
+}
