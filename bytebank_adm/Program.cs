7a7638aefbbd6cdd3927a4b56da15a4b134493c6
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-01 22:05:46 -0300

    Inseridos: total de funcionários e o prêmio semestral

--- bytebank_adm/Program.cs
+++ bytebank_adm/Program.cs
@@ -6,7 +6,7 @@ pedro.Nome = "Pedro Malazartes";
 pedro.Cpf = "123456789-00";
 pedro.Salario = 2000.0;
 
-Console.WriteLine($"Nome: {pedro.Nome}\nCPF: {pedro.Cpf}\nSalário: {String.Format("{0:0.00}", pedro.Salario)}\nBonificação: {String.Format("{0:0.00}", pedro.GetBonificacao())}");
+Console.WriteLine($"Nome: {pedro.Nome}\nCPF: {pedro.Cpf}\nSalário: {String.Format("{0:0.00}", pedro.Salario)}\nBonificação: {String.Format("{0:0.00}", pedro.GetBonificacao())}\nPrêmio semestral: {String.Format("{0:0.00}", pedro.PremioSemestral())}");
 
 Diretor roberta = new Diretor();
 roberta.Nome = "Roberta Silva";
@@ -14,13 +14,13 @@ roberta.Cpf = "987654321-00";
 roberta.Salario = 5000.0;
 
 Console.WriteLine();
-Console.WriteLine($"Nome: {roberta.Nome}\nCPF: {roberta.Cpf}\nSalário: {String.Format("{0:0.00}", roberta.Salario)}\nBonificação: {String.Format("{0:0.00}", roberta.GetBonificacao())}");
+Console.WriteLine($"Nome: {roberta.Nome}\nCPF: {roberta.Cpf}\nSalário: {String.Format("{0:0.00}", roberta.Salario)}\nBonificação: {String.Format("{0:0.00}", roberta.GetBonificacao())}\nPrêmio semestral: {String.Format("{0:0.00}", roberta.PremioSemestral())}");
 
 GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 gerenciador.Registrar(pedro);
 gerenciador.Registrar(roberta);
 
 Console.WriteLine();
-Console.WriteLine($"Total de bonificações: {String.Format("{0:0.00} ", gerenciador.TotalBonificacao)}");
+Console.WriteLine($"Total de bonificações: {String.Format("{0:0.00} ", gerenciador.TotalBonificacao)}\nTotal de funcionários: {Funcionario.TotalFuncionarios}");
 
 Console.ReadKey();
