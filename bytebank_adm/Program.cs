2a5fe25d985f6161e86545d09becbb97b6d5cdd4
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-02 17:02:36 -0300

    Alterações
    -Passando os parâmetros obrigatórios ao criar o objeto;
    -Remoção de incompatibilidades no código;
    -Teste do método AumentarSalario (funcionário e diretor)
    -Print do método AumentarSalario para ver se foi implementado
    corretamente.

--- bytebank_adm/Program.cs
+++ bytebank_adm/Program.cs
@@ -1,17 +1,13 @@
 ﻿using bytebank_adm.Funcionarios;
 using bytebank_adm.Utilitario;
 
-Funcionario pedro = new Funcionario();
+Funcionario pedro = new Funcionario("123456789-00", 2000.0);
 pedro.Nome = "Pedro Malazartes";
-pedro.Cpf = "123456789-00";
-pedro.Salario = 2000.0;
 
 Console.WriteLine($"Nome: {pedro.Nome}\nCPF: {pedro.Cpf}\nSalário: {String.Format("{0:0.00}", pedro.Salario)}\nBonificação: {String.Format("{0:0.00}", pedro.GetBonificacao())}\nPrêmio semestral: {String.Format("{0:0.00}", pedro.PremioSemestral())}");
 
-Diretor roberta = new Diretor();
+Diretor roberta = new Diretor("987654321-00", 5000.0);
 roberta.Nome = "Roberta Silva";
-roberta.Cpf = "987654321-00";
-roberta.Salario = 5000.0;
 
 Console.WriteLine();
 Console.WriteLine($"Nome: {roberta.Nome}\nCPF: {roberta.Cpf}\nSalário: {String.Format("{0:0.00}", roberta.Salario)}\nBonificação: {String.Format("{0:0.00}", roberta.GetBonificacao())}\nPrêmio semestral: {String.Format("{0:0.00}", roberta.PremioSemestral())}");
@@ -22,5 +18,10 @@ gerenciador.Registrar(roberta);
 
 Console.WriteLine();
 Console.WriteLine($"Total de bonificações: {String.Format("{0:0.00} ", gerenciador.TotalBonificacao)}\nTotal de funcionários: {Funcionario.TotalFuncionarios}");
+
+pedro.AumentarSalario();
+roberta.AumentarSalario();
+
+Console.WriteLine($"Novo valor do salário do Pedro: {String.Format("{0:0,00}", pedro.Salario)}\nNovo valor do salário da Roberta: {String.Format("{0:0,00}", roberta.Salario)}");
 
 Console.ReadKey();
