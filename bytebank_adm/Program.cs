0a6b83554096201555ef488ff450981f87f081e8
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-03 14:00:52 -0300

    Definição dos novos objetos e cálculo da bonificação total

--- bytebank_adm/Program.cs
+++ bytebank_adm/Program.cs
@@ -1,27 +1,54 @@
 ﻿using bytebank_adm.Funcionarios;
 using bytebank_adm.Utilitario;
 
-Funcionario pedro = new Funcionario("123456789-00", 2000.0);
-pedro.Nome = "Pedro Malazartes";
+#region
+//Funcionario pedro = new Funcionario("123456789-00", 2000.0);
+//pedro.Nome = "Pedro Malazartes";
 
-Console.WriteLine($"Nome: {pedro.Nome}\nCPF: {pedro.Cpf}\nSalário: {String.Format("{0:0.00}", pedro.Salario)}\nBonificação: {String.Format("{0:0.00}", pedro.GetBonificacao())}\nPrêmio semestral: {String.Format("{0:0.00}", pedro.PremioSemestral())}");
+//Console.WriteLine($"Nome: {pedro.Nome}\nCPF: {pedro.Cpf}\nSalário: {String.Format("{0:0.00}", pedro.Salario)}\nBonificação: {String.Format("{0:0.00}", pedro.GetBonificacao())}\nPrêmio semestral: {String.Format("{0:0.00}", pedro.PremioSemestral())}");
 
-Diretor roberta = new Diretor("987654321-00", 5000.0);
-roberta.Nome = "Roberta Silva";
+//Diretor roberta = new Diretor("987654321-00", 5000.0);
+//roberta.Nome = "Roberta Silva";
 
-Console.WriteLine();
-Console.WriteLine($"Nome: {roberta.Nome}\nCPF: {roberta.Cpf}\nSalário: {String.Format("{0:0.00}", roberta.Salario)}\nBonificação: {String.Format("{0:0.00}", roberta.GetBonificacao())}\nPrêmio semestral: {String.Format("{0:0.00}", roberta.PremioSemestral())}");
+//Console.WriteLine();
+//Console.WriteLine($"Nome: {roberta.Nome}\nCPF: {roberta.Cpf}\nSalário: {String.Format("{0:0.00}", roberta.Salario)}\nBonificação: {String.Format("{0:0.00}", roberta.GetBonificacao())}\nPrêmio semestral: {String.Format("{0:0.00}", roberta.PremioSemestral())}");
 
-GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
-gerenciador.Registrar(pedro);
-gerenciador.Registrar(roberta);
+//GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+//gerenciador.Registrar(pedro);
+//gerenciador.Registrar(roberta);
 
-Console.WriteLine();
-Console.WriteLine($"Total de bonificações: {String.Format("{0:0.00} ", gerenciador.TotalBonificacao)}\nTotal de funcionários: {Funcionario.TotalFuncionarios}");
+//Console.WriteLine();
+//Console.WriteLine($"Total de bonificações: {String.Format("{0:0.00} ", gerenciador.TotalBonificacao)}\nTotal de funcionários: {Funcionario.TotalFuncionarios}");
 
-pedro.AumentarSalario();
-roberta.AumentarSalario();
+//pedro.AumentarSalario();
+//roberta.AumentarSalario();
 
-Console.WriteLine($"Novo valor do salário do Pedro: {String.Format("{0:0,00}", pedro.Salario)}\nNovo valor do salário da Roberta: {String.Format("{0:0,00}", roberta.Salario)}");
+//Console.WriteLine($"Novo valor do salário do Pedro: {String.Format("{0:0,00}", pedro.Salario)}\nNovo valor do salário da Roberta: {String.Format("{0:0,00}", roberta.Salario)}");
+#endregion
 
-Console.ReadKey();
+void CalcularBonificacao(){
+    GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+    Designer ulisses = new Designer("14725836999");
+    ulisses.Nome = "Ulisses Souza";
+
+    Diretor paula = new Diretor("96314785234");
+    paula.Nome = "Paula Souza";
+
+    Auxiliar igor = new Auxiliar("258469173-33");
+    igor.Nome = "Igor Dias";
+
+    GerenteDeContas camila = new GerenteDeContas("654987321-00");
+    camila.Nome = "Camila Oliveira";
+
+    gerenciador.Registrar(camila);
+    gerenciador.Registrar(igor);
+    gerenciador.Registrar(paula);
+    gerenciador.Registrar(ulisses);
+
+    Console.WriteLine($"Total de bonificação: {String.Format("{0:0.00}", gerenciador.TotalBonificacao)}");
+
+    Console.ReadKey();
+}
+
+CalcularBonificacao();
