6a706c64f5e0a5ca7bad55f707c706cb2b4c26d4
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-03 15:17:56 -0300

    Alterando a herança da classe GerenteDeContas

--- bytebank_adm/Funcionarios/GerenteDeContas.cs
+++ bytebank_adm/Funcionarios/GerenteDeContas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bytebank_adm.SistemaInterno;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace bytebank_adm.Funcionarios
 {
-    public class GerenteDeContas: Funcionario
+    public class GerenteDeContas: Autenticavel
     {
         public GerenteDeContas(string cpf) : base(cpf, 4000) 
         {        
@@ -17,9 +18,19 @@ namespace bytebank_adm.Funcionarios
             return this.Salario * 0.25;
         }
 
+        public override double PremioSemestral()
+        {
+            return this.Salario * 0.3;
+        }
+
         public override void AumentarSalario()
         {
             this.Salario *= 1.05;
+        }
+
+        public override bool Autenticar(string senha, string login)
+        {
+            return this.Senha == senha && this.Login == login;
         }
     }
 }
