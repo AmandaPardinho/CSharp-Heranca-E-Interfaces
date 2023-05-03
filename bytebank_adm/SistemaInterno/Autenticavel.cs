bb35d740322ce1c1e0572e226f07bce938263ff4
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-03 15:16:04 -0300

    Criação da classe SistemaInterno

---
+++ bytebank_adm/SistemaInterno/Autenticavel.cs
@@ -0,0 +1,22 @@
+﻿using bytebank_adm.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_adm.SistemaInterno
+{
+    public abstract class Autenticavel : Funcionario
+    {
+        public string Senha { get; set; }
+
+        public string Login { get; set; }
+
+        public abstract bool Autenticar(string senha, string login);
+
+        public Autenticavel(string cpf, double salario): base(cpf, salario)
+        {
+        }
+    }
+}
