cdc828a75a9a3d1959fb5c5626abae989f859153
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-03 14:10:03 -0300

    Criação de novo projeto para mostrar como criar um construtor que obrigue a passar alguns parâmetros

---
+++ Biblioteca/Livro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Livro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public double Isbn { get; set; }
+        public int AnoPublicacao { get; set; }
+        public string EstiloLiterario { get; set; }
+        public int NumPaginas { get; set; }
+        public string PaisPublicacao { get; set; }
+
+        public Livro(double isbn, string titulo)
+        {
+            this.Isbn = isbn;
+            this.Titulo = titulo;
+        }
+    }
+}
