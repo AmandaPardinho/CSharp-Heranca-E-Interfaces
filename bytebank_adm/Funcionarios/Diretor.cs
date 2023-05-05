ea7434ba956e662a49cab6a2dd7fa3b7438fb0c9
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-05 10:22:06 -0300

    Retirada dos testes feitos com a interface nessa classe

--- bytebank_adm/Funcionarios/Diretor.cs
+++ bytebank_adm/Funcionarios/Diretor.cs
@@ -10,17 +10,7 @@ namespace bytebank_adm.Funcionarios
     public class Diretor: FuncionarioAutenticavel
     {
         //A palavra "override" mostra que esse método irá sobrescrever o método da superclasse que estiver definido como "virtual"
-        public override double GetBonificacao()
-        {
-            //Palavra "base" => palavra reservada que permite acessar implementações e propriedades existentes na superclasse
-            return this.Salario * 0.5;
-        }
-
-        public override double PremioSemestral()
-        {
-            return this.Salario * 0.5;
-        }
-
+        
         public Diretor(string cpf): base(cpf, 5000)
         {
             
