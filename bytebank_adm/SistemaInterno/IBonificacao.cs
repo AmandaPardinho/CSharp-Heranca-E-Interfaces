4099c3311ab063ff85419c3470d26caf3560f527
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-05 10:22:53 -0300

    Exclusão de um dos métodos e mudança na assinatura

---
+++ bytebank_adm/SistemaInterno/IBonificacao.cs
@@ -0,0 +1,16 @@
+﻿using bytebank_adm.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_adm.SistemaInterno
+{
+    public interface IBonificacao
+    {
+        public double PorcentagemBonus { get; set; }
+
+        public double SalarioMaisBonus(Funcionario funcionario, double porcentagemBonus);
+    }
+}
